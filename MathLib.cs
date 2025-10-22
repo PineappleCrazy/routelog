003a2883087ea11e050387fd86a0a2771f93f31f
PineappleCrazy <139810771+PineappleCrazy@users.noreply.github.com>
2025-10-22 11:51:02 +0100

    Added c# test files

---
+++ MathLib.cs
@@ -0,0 +1,10 @@
+namespace MathLib
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ calc.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+python "calculator.py"
+pause
